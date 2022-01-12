1bb86458ca229699c174e591c099253d0b5f774f
yppyrz <yppyrz@gmail.com>
2022-01-12 19:00:02 +0300

    Product updated

---
+++ NbuyGetir.Common/Url/UrlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NbuyGetir.Common.Url
+{
+    public static class UrlHelper
+    {
+        public static bool IsUrl(string url)
+        {
+            string pattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[azA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
+
+            var regex = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(url);
+        }
+
+    }
+}
--- NbuyGetir.Domain/Models/Product.cs
+++ NbuyGetir.Domain/Models/Product.cs
@@ -1,8 +1,10 @@
-﻿using NbuyGetir.Core.Entities;
+﻿using NbuyGetir.Common.Url;
+using NbuyGetir.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NbuyGetir.Domain.Models
@@ -15,26 +17,35 @@ namespace NbuyGetir.Domain.Models
         public decimal ListPrice { get; private set; } // satış fiyatı
         public int Stock { get; private set; } // current stock
         public string Description { get; private set; } // 10x2, 1lt, 2kg vb.
-        public string ImageBase64 { get; private set; }
         public string ImageUrl { get; private set; }
         public decimal DiscountedListPrice { get; set; }
         public bool IsDiscounted { get {
                 return DiscountedListPrice < ListPrice ? true : false;
+        } }
+        public bool IsStockInCriticalLevel { get {
+                return Stock < 10 ? true : false;
             } }
 
-
-        public Product(string name, decimal unitPrice, decimal listPrice, int stock, string description, string ımageBase64, string ımageUrl)
+        public Product(string name, decimal unitPrice, decimal listPrice, int stock,decimal discountedListPrice, string description, string ımageBase64, string ımageUrl)
         {
-            Name = name;
-            UnitPrice = unitPrice;
-            ListPrice = listPrice;
-            Stock = stock;
-            Description = description;
-            ImageBase64 = ımageBase64;
-            ImageUrl = ımageUrl;
+            SetName(name);
+            SetPrice(unitPrice, listPrice, discountedListPrice);
+            SetDescription(description);
+            SetStock(stock);
         }
-
-        private void SetName(string name)
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new Exception("Ürün açıklama alanını doldurunuz");
+            }
+            if (description.Length > 50)
+            {
+                throw new Exception("Maksimum 50 karakter girebilirsiniz");
+            }
+            Description = description.Trim();
+        }
+        public void SetName(string name)
         {
             if (string.IsNullOrEmpty(name))
             {
@@ -43,20 +54,31 @@ namespace NbuyGetir.Domain.Models
             Name = name.Trim();
         }
 
-        private void SetPrice(decimal unitPrice, decimal listPrice, double discountAmount)
+        public void SetPrice(decimal unitPrice, decimal listPrice, decimal discountedListPrice)
         {
             if (unitPrice > listPrice)
             {
                 throw new Exception("Birim fiyat liste fiyatından büyük olamaz");
-
             }
-            if (unitPrice <= 0 || listPrice <= 0)
+            
+            if (unitPrice <= 0 || listPrice <= 0 || discountedListPrice <= 0)
             {
-                throw new Exception("Ürün birim fiyatı veya ürün satış fiyatı negatif veya 0 verilemez");
+                throw new Exception("Ürün birim fiyatı veya ürün satış fiyatı negatif veya 0 verilemez, indirimli satış fiyatı 0 veya daha küçük olamaz.");
+            }
+            
+            if (discountedListPrice > listPrice)
+            {
+                throw new Exception("İndirimli satış fiyatı satış fiyatından büyük olamaz.");
+            }
+            
+            if (discountedListPrice < unitPrice)
+            {
+                throw new Exception("İndirimli satış fiyatı birim fiyattan küçük olamaz");
             }
 
             ListPrice = listPrice;
             UnitPrice = unitPrice;
+            DiscountedListPrice = discountedListPrice;
         }
 
 
@@ -88,5 +110,63 @@ namespace NbuyGetir.Domain.Models
             DiscountedListPrice = newDiscountedListPrice;
         }
         
+        private void SetStock(int stock)
+        {
+            if (stock < 0)
+            {
+                throw new Exception("Stok değeri 0'dan daha küçük olamaz");
+            }
+            if (stock < 10)
+            {
+                // Kritik stok seviyesinin altındadır. Ürün işaretlenecektir.
+            }
+            else
+            {
+
+            }
+        }
+
+        public void StockIn(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new Exception("stoğa eklenecek miktar adedi 0 ya da daha düşük olamaz.");
+            }
+            Stock += quantity;
+        }
+        public void StockOut(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new Exception("stoktan düşülecek ürün adedi 0 ya da daha düşük olamaz.");
+            }
+            if (IsStockInCriticalLevel)
+            {
+                // event fırlat
+            }
+            if (quantity > Stock)
+            {
+                // hatalı işlem
+                throw new Exception("stoktan düşülen miktar stok değerinden büyük olamaz");
+            }
+
+            Stock -= quantity;
+        }
+        public void SetImageUrl(string imageUrl)
+        {
+            if (!UrlHelper.IsUrl(imageUrl))
+            {
+                throw new Exception("resim yolu url formatında değildir.");
+            }
+
+            if (string.IsNullOrEmpty(imageUrl))
+            {
+                ImageUrl = "default-product.jpeg";
+            }
+            else
+            {
+                ImageUrl = imageUrl.Trim();
+            }
+        }
     }
 }
